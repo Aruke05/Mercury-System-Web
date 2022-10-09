9c31721c703b4f2728b51d1f326ff07e53757956
Yu <enuimba@gmail.com>
2022-10-09 11:46:56 +0800

    随机个人信息

---
+++ src/views/space/randomPerson/module/personCard.vue
@@ -0,0 +1,77 @@
+<template>
+  <el-card class="box-card" style="min-height: 340px">
+    <div slot="header" class="clearfix">
+      <span>{{ person.chineseName }}</span>
+      <el-button class="copy" style="float: right; padding: 3px 0" type="text" :data-clipboard-text="JSON.stringify(this.person)" @click="handleClipboard()">复制json</el-button>
+    </div>
+    <div class="text item">
+      中文名: {{ person.chineseName }}
+    </div>
+    <div class="text item">
+      年龄: {{ person.age }}
+    </div>
+    <div class="text item">
+      性别: {{ person.sex }}
+    </div>
+    <div class="text item">
+      生日: {{ person.birth }}
+    </div>
+    <div class="text item">
+      地址: {{ person.address }}
+    </div>
+    <div class="text item">
+      邮箱: {{ person.email }}
+    </div>
+    <div class="text item">
+      身份证号码: {{ person.idCardNo }}
+    </div>
+    <div class="text item">
+      昵称: {{ person.nickName }}
+    </div>
+    <div class="text item">
+      QQ号码: {{ person.qQNumber }}
+    </div>
+    <div class="text item">
+      英文名: {{ person.englishName }}
+    </div>
+    <div class="text item">
+      公网IP: {{ person.publicIpv4 }}
+    </div>
+    <div class="text item">
+      私网IP: {{ person.privateIpv4 }}
+    </div>
+  </el-card>
+</template>
+
+<script >
+import Clipboard from 'clipboard'
+
+export default {
+  name: 'PersonCard',
+  props: {
+    person: {
+      type: Object
+    }
+  },
+  methods: {
+    handleClipboard() {
+      var clipboard = new Clipboard('.copy')
+      clipboard.on('success', e => {
+        this.$message.success('复制成功')
+        // 释放内存
+        clipboard.destroy()
+      })
+      clipboard.on('error', e => {
+        // 不支持复制
+        this.$message.error('该浏览器不支持自动复制')
+        // 释放内存
+        clipboard.destroy()
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/space/randomPerson/randomPerson.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-editor-container">
+      <el-row style="background:#fff;padding:16px 16px 0;margin-top: 10px;margin-bottom:20px;">
+        <el-input-number
+          v-model="queryParam.num"
+          clearable
+          size="small"
+          placeholder="输入个数"
+          style="width: 200px;margin-bottom: 10px"
+          :controls="false"
+          class="filter-item"
+        />
+        <el-button class="filter-item" size="mini" type="success" icon="el-icon-search" @click="query()">获取</el-button>
+      </el-row>
+      <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px; min-height: 100px;">
+        <el-col v-for="item in this.dataList" :span="6">
+          <PersonCard :person="item" /></el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import { postAction } from '@/api/manage'
+import PersonCard from '@/views/space/randomPerson/module/personCard'
+
+export default {
+  name: 'RandomPerson',
+  components: { PersonCard },
+  data() {
+    return {
+      queryParam: {},
+      url: {
+        randomPerson: '/space/randomPerson'
+      },
+      dataList: []
+    }
+  },
+  methods: {
+    query() {
+      if (!this.queryParam.num) {
+        this.$message.error('?')
+        return
+      }
+      postAction(this.url.randomPerson + '?num=' + this.queryParam.num, {}).then(res => {
+        if (res.code === 200) {
+          this.dataList = res.result
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.dashboard-editor-container {
+  padding: 18px 22px 22px 22px;
+  background-color: rgb(240, 242, 245);
+.chart-wrapper {
+  background: #fff;
+  padding: 16px 16px 0;
+  margin-bottom: 32px;
+}
+}
+</style>
